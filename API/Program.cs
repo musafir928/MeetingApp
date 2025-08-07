f3aed833523add8185174a8c0af97faff719d857
Adil Ablimit <Adil.ablimit@ortec.com>
2025-08-07 14:44:55 +0200

    feat: backend: add CORS

--- API/Program.cs
+++ API/Program.cs
@@ -7,13 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
-{ 
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// CORS
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x =>
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200")
+);
 app.MapControllers();
 
 app.Run();
