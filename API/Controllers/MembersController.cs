3c24a768ba5200fbd1f6920f8d4ea0097922648f
Adil Ablimit <Adil.ablimit@ortec.com>
2025-08-15 20:31:23 +0200

    feat: end of section 4

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,11 +1,11 @@
+using API.Controllers;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-[Route("api/[controller]")]
-[ApiController]
-public class MembersController(AppDbContext context) : ControllerBase
+public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<AppUser>>> GetUsers()
@@ -14,6 +14,7 @@ public class MembersController(AppDbContext context) : ControllerBase
         return members;
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(string id)
     {
---
+++ readme.md
@@ -0,0 +1,5 @@
+### to ignore azure artifacts
+
+
+`env -u NUGET_PLUGIN_PATHS -u NUGET_CREDENTIALPROVIDERS_PATH dotnet restore`
+
