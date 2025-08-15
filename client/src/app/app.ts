6a4434b3a47ff5a865afa9eb4589210655fc1698
Adil Ablimit <Adil.ablimit@ortec.com>
2025-08-16 00:06:20 +0200

    feat: client login: basic function

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,30 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from '../layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
-
-  private http =  inject(HttpClient);
+  private http = inject(HttpClient);
   protected readonly title = 'Meeting app';
   protected members = signal<any>([]);
-  
-  async ngOnInit(){
+
+  async ngOnInit() {
     this.members.set(await this.getMembers());
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get("https://localhost:5001/api/members"));
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
       throw error;
     }
   }
-
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
+# LIFT Coding style
+
+- <u>L</u>ocate
+- <u>I</u>dentify - file naming should describe what the file includes
+- <u>F</u>lat - do not make folders too long
+- <u>T</u>ry to be DRY - Do not Repeat Yourself
+
 ### to ignore azure artifacts
 
 
 `env -u NUGET_PLUGIN_PATHS -u NUGET_CREDENTIALPROVIDERS_PATH dotnet restore`
+
+## CSS Guide
+
+### flex system
+[flex box froggy - visual demonstration of css flex](https://flexboxfroggy.com) 
+
+### icons
+[Hero icons](https://heroicons.com)
 
