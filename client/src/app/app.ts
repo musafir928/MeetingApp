a7f24698b4bd0558f4e9c68acb912824ca5fd331
Adil Ablimit <Adil.ablimit@ortec.com>
2025-08-20 13:20:42 +0200

    feat: :sparkles: add basic routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,12 +3,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
-import { Home } from '../features/home/home';
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -17,6 +17,7 @@ export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = 'Meeting app';
   protected members = signal<User[]>([]);
+  protected router = inject(Router);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
